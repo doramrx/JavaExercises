16b952286268d7ec96c2ea85152e4a52a4d46e83
Isadora <isah3027@gmail.com>
2023-10-29 11:44:21 -0300

    Delete data from database

--- JavaExercises/src/main/java/TryDatabaseConnection/DeleteData.java
+++ JavaExercises/src/main/java/TryDatabaseConnection/DeleteData.java
@@ -1,2 +1,35 @@
-package TryDatabaseConnection;public class DeleteData {
+package TryDatabaseConnection;
+
+import Database.DB;
+import Database.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteData {
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DB.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "delete from department "
+                    + "where "
+                    + "Id = ?");
+
+            preparedStatement.setInt(1, 5);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+    }
 }
+
