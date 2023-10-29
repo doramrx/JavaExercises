d83e85c4aabe0569988e76ad57c8e0bc9fc1da66
IgorIA <igorsemphoski@gmail.com>
2023-10-28 23:09:30 -0300

    move input reading logic to data collector class

--- JavaExercises/src/main/java/VoteCountSystem/DataCollector.java
+++ JavaExercises/src/main/java/VoteCountSystem/DataCollector.java
@@ -1,19 +1,50 @@
 package VoteCountSystem;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DataCollector {
     Scanner sc = new Scanner(System.in);
     public String filePath;
 
-    public void collectData() {
+    public Map<String, Integer> collectData() {
         System.out.print("Enter file full path: ");
         this.filePath = this.sc.nextLine();
+
+        return this.readVotesOfPeople();
     }
 
-    public String getFilePath(){
-        return this.filePath;
+    private Map<String, Integer> readVotesOfPeople() {
+        Map<String, Integer> votesByPeopleList = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(this.filePath))){
+            String line = br.readLine();
+            while (line != null) {
+
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
+
+                if(votesByPeopleList.containsKey(name)){
+                    int votes = votesByPeopleList.get(name);
+                    votesByPeopleList.put(name, count + votes);
+                } else {
+                    votesByPeopleList.put(name, count);
+                }
+
+                line = br.readLine();
+            }
+
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return votesByPeopleList;
     }
+
 }
 
 //C:\\Users\\Acer\\Desktop\\ProgramaçãoJava\\Workspace\\JavaExercises\\src\\main\\java\\VoteCountSystem\\in.csv
