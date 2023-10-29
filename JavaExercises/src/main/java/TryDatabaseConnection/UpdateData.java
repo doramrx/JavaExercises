5e8aabd42f3f9fcb029b41c1743818bfa0c3d2fa
Isadora <isah3027@gmail.com>
2023-10-29 11:43:55 -0300

    Update data from database

--- JavaExercises/src/main/java/TryDatabaseConnection/UpdateData.java
+++ JavaExercises/src/main/java/TryDatabaseConnection/UpdateData.java
@@ -1,2 +1,35 @@
-package TryDatabaseConnection;public class UpdateData {
+package TryDatabaseConnection;
+
+import Database.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UpdateData {
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DB.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "update seller "
+                    + "set BaseSalary = BaseSalary + ? "
+                    + "where "
+                    + "(DepartmentId = ?)");
+
+            preparedStatement.setDouble(1, 200.0);
+            preparedStatement.setInt(2, 2);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+    }
 }
