eea3e0185d625426d5ad9855cb95d987997e82de
IgorIA <igorsemphoski@gmail.com>
2023-10-28 21:18:22 -0300

    move input reading logi to DataCollector class

--- JavaExercises/src/main/java/WebSiteLogSystem/DataCollector.java
+++ JavaExercises/src/main/java/WebSiteLogSystem/DataCollector.java
@@ -1,18 +1,46 @@
 package WebSiteLogSystem;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.Set;
 
 public class DataCollector {
     Scanner sc = new Scanner(System.in);
-    public String filePath;
+    private String filePath;
 
-    public void collectData() {
+    public void collectData(Set<LogEntry> logEntries) {
         System.out.print("Enter file full path: ");
         this.filePath = this.sc.nextLine();
+        this.readLogsToSetList(logEntries);
     }
 
     public String getFilePath(){
         return this.filePath;
+    }
+
+    private void readLogsToSetList(Set<LogEntry> logEntries) {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+            while (line != null) {
+
+                String[] fields =  line.split(" ");
+                String username = fields[0];
+                String stringDate = fields[1];
+                LocalDateTime date = LocalDateTime.parse(stringDate, formatter);
+
+                logEntries.add(new LogEntry(username, date));
+
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
 
--- JavaExercises/src/main/java/WebSiteLogSystem/LogSystem.java
+++ JavaExercises/src/main/java/WebSiteLogSystem/LogSystem.java
@@ -1,41 +1,19 @@
 package WebSiteLogSystem;
-import java.io.*;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class LogSystem {
-    Set<LogEntry> users = new HashSet<>();
+    private Set<LogEntry> users = new HashSet<>();
 
     void init() {
-
        DataCollector dataCollector = new DataCollector();
-       dataCollector.collectData();
+       dataCollector.collectData(users);
 
        System.out.println("Total users: " + countUsers(dataCollector.getFilePath()));
-
     }
 
     public Integer countUsers(String filePath){
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-
-            String line = br.readLine();
-            while (line != null) {
-
-                String[] fields =  line.split(" ");
-                String username = fields[0];
-                String stringDate = fields[1];
-                LocalDateTime date = LocalDateTime.parse(stringDate, formatter);
-
-                this.users.add(new LogEntry(username, date));
-
-                line = br.readLine();
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
         return this.users.size();
     }
 }
